acb7affa40233d3ddd1c99c91248b811d5890dd7
ekolpakov <ekolpakov@greet-go.com>
2021-11-19 16:21:48 +0600

    template: 012 подкорректировал шаблоны

--- plugin/src/kz/greetgo/mybpm/plugin/template/TestSchedulerController.java
+++ plugin/src/kz/greetgo/mybpm/plugin/template/TestSchedulerController.java
@@ -8,7 +8,7 @@ import kz.greetgo.scheduling.Scheduled;
 public class TestSchedulerController {
 
   @FromConfig("test wow")
-  @Scheduled("repeat every 15 seconds")
+  @Scheduled("repeat every 30 seconds")
   public void ping() {
     System.out.println("tO2NV50Qrm :: Ping from scheduler " + new Date());
   }
